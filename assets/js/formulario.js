09c5e86142110e37f0e3112d2eae69fc61f97555
Matias98cs <matias98cs@gmail.com>
2022-08-13 19:23:48 -0300

    cambios en la funcionalidad y el carrito

--- assets/js/formulario.js
+++ assets/js/formulario.js
@@ -1,11 +1,28 @@
 const formulario = document.querySelector('#formulario');
-
-
+const btnEmail = document.querySelector('#email');
+const btnPass = document.querySelector('#pass');
+const mensaje = document.querySelector('#mensaje');
 
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
-    const emailInpu = document.querySelector('#email');
-    const passInpu = document.querySelector('#pass');
-    console.log(emailInpu);
-    console.log(passInpu)
+    console.log(btnEmail.value)
+    if(btnEmail.vallue != "" && btnPass.value != ""){
+        location.replace("products.html")
+
+    }else {
+        mostrarMensaje('Ingrese sus datos en ambos campos')
+    }
 })
+
+function mostrarMensaje(msj){
+    mensaje.innerHTML = "";
+
+    const parrafo = document.createElement('p');
+    parrafo.classList.add('text-center', 'fs-3', 'text-mensaje', 'p-4')
+    parrafo.innerHTML = msj
+    mensaje.appendChild(parrafo)
+
+    setTimeout(() => {
+        parrafo.remove()
+    }, 2500);
+}
